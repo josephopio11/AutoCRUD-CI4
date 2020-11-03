d3bc37b60b0c56cb325459cb26b25b2d3725d766
Joseph Opio <josephopio11@gmail.com>
2020-11-03 14:40:06 +0300

    main

--- app/Views/admin/cmps/pagination.php
+++ app/Views/admin/cmps/pagination.php
@@ -1,36 +1,36 @@
-<?php $pager->setSurroundCount(2) ?>
+<?php $pager->setSurroundCount(2)?>
 <ul class="pagination pagination-sm m-0 float-right">
-    <?php if ($pager->hasPrevious()) : ?>
+    <?php if ($pager->hasPrevious()): ?>
         <li class="page-item">
-            <a href="<?= $pager->getFirst() ?>" class="page-link" aria-label="<?= lang('Pager.first') ?>">
-                <span aria-hidden="true"><?= lang('Pager.first') ?></span>
+            <a href="<?=$pager->getFirst()?>" class="page-link" aria-label="<?=lang('Pager.first')?>">
+                <span aria-hidden="true"><?=lang('Pager.first')?></span>
             </a>
         </li>
         <li class="page-item">
-            <a href="<?= $pager->getPrevious() ?>" class="page-link" aria-label="<?= lang('Pager.previous') ?>">
-                <span aria-hidden="true"><?= lang('Pager.previous') ?></span>
+            <a href="<?=$pager->getPrevious()?>" class="page-link" aria-label="<?=lang('Pager.previous')?>">
+                <span aria-hidden="true"><?=lang('Pager.previous')?></span>
             </a>
         </li>
-    <?php endif ?>
+    <?php endif?>
 
-    <?php foreach ($pager->links() as $link) : ?>
-        <li class="page-item <?= $link['active'] ? ' active ' : '' ?>">
-            <a class="page-link" href="<?= $link['uri'] ?>">
-                <?= $link['title'] ?>
+    <?php foreach ($pager->links() as $link): ?>
+        <li class="page-item <?=$link['active'] ? ' active ' : ''?>">
+            <a class="page-link" href="<?=$link['uri']?>">
+                <?=$link['title']?>
             </a>
         </li>
-    <?php endforeach ?>
+    <?php endforeach?>
 
-    <?php if ($pager->hasNext()) : ?>
+    <?php if ($pager->hasNext()): ?>
         <li class="page-item">
-            <a class="page-link" href="<?= $pager->getNext() ?>" aria-label="<?= lang('Pager.next') ?>">
-                <span aria-hidden="true"><?= lang('Pager.next') ?></span>
+            <a class="page-link" href="<?=$pager->getNext()?>" aria-label="<?=lang('Pager.next')?>">
+                <span aria-hidden="true"><?=lang('Pager.next')?></span>
             </a>
         </li>
         <li class="page-item">
-            <a class="page-link" href="<?= $pager->getLast() ?>" aria-label="<?= lang('Pager.last') ?>">
-                <span aria-hidden="true"><?= lang('Pager.last') ?></span>
+            <a class="page-link" href="<?=$pager->getLast()?>" aria-label="<?=lang('Pager.last')?>">
+                <span aria-hidden="true"><?=lang('Pager.last')?></span>
             </a>
         </li>
-    <?php endif ?>
+    <?php endif?>
 </ul>
