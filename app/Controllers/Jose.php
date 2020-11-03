1186c20958f3342f1da0ed6473cbdf8bd62ca14d
Joseph Opio <josephopio11@gmail.com>
2020-11-03 11:00:21 +0300

    main

---
+++ app/Controllers/Jose.php
@@ -0,0 +1,12 @@
+<?php namespace App\Controllers;
+
+class Jose extends BaseController
+{
+	public function index()
+	{
+		return view('admin/layouts/main');
+	}
+
+	//--------------------------------------------------------------------
+
+}
